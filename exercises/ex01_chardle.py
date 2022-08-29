662a5402bec46c8f872d1505504fc4ca738f8a73
Victor Hazboun <vhazboun@unc.edu>
2022-08-28 22:38:46 -0400

    Progress on Exercises 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,17 +1,17 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 
-_author_ = "730465342"
+__author__ = "730465342"
 
 five_letter: str = input("Enter a 5-character word: ")
 
 if len(five_letter) == 5:
     one_character: str = input("Enter a single character: ")
 else:
-        print("Error: Word must contain 5 characters")
-        exit()
+    print("Error: Word must contain 5 characters")
+    exit()
 if len(one_character) != 1:
-        print("Error: Character must be a single character.")
-        exit()
+    print("Error: Character must be a single character.")
+    exit()
 
 print("Searching for " + one_character + " in " + five_letter)
 
@@ -46,9 +46,3 @@ else:
         print(str(matchy) + " instances of " + one_character + " found in " + five_letter)
     else:
         print("No instances of " + one_character + " found in " + five_letter)
-
-
-
-
-
-
